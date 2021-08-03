f777ee170404f69409893743cec6aa31908d6558
geod <foo@boo.com>
2021-08-03 12:07:53 -0700

    find .

--- startuptoolbag/pipeline/cdk_pipeline_stack.py
+++ startuptoolbag/pipeline/cdk_pipeline_stack.py
@@ -57,7 +57,7 @@ class CDKPipelineStack(core.Stack):
             source_artifact=post_react_build_artifact,
             cloud_assembly_artifact=cloud_assembly_artifact,
             install_command='npm install -g aws-cdk && pip install -r requirements.txt',
-            synth_command='find . && cd $CODEBUILD_SRC_DIR && cdk synth',
+            synth_command='find .',
             additional_artifacts=[{'artifact': application_code, 'directory': './'}])
 
 
