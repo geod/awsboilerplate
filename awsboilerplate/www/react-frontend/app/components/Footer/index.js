85716f714da7fad02e3e1e0f8ffb1a7e54a89262
geod <foo@boo.com>
2021-08-05 22:03:41 -0700

    git status Change 10:03

--- awsboilerplate/www/react-frontend/app/components/Footer/index.js
+++ awsboilerplate/www/react-frontend/app/components/Footer/index.js
@@ -6,6 +6,7 @@ import LocaleToggle from 'containers/LocaleToggle';
 import Wrapper from './Wrapper';
 import messages from './messages';
 
+
 function Footer() {
   return (
     <Wrapper>
@@ -16,12 +17,8 @@ function Footer() {
         <LocaleToggle />
       </section>
       <section>
-        <FormattedMessage
-          {...messages.authorMessage}
-          values={{
-            author: <A href="https://twitter.com/mxstbr">Max Stoiber</A>,
-          }}
-        />
+        <p>
+        </p>
       </section>
     </Wrapper>
   );
--- awsboilerplate/www/react-frontend/app/containers/HomePage/messages.js
+++ awsboilerplate/www/react-frontend/app/containers/HomePage/messages.js
@@ -10,7 +10,7 @@ export const scope = 'boilerplate.containers.HomePage';
 export default defineMessages({
   startProjectHeader: {
     id: `${scope}.start_project.header`,
-    defaultMessage: 'awsboilerplate.io Live Demo!!',
+    defaultMessage: 'awsboilerplate.io Live Demo!!!',
   },
   startProjectMessage: {
     id: `${scope}.start_project.message`,
